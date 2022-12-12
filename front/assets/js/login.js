e64304a7d41d0f3c5782c1ec0e9058e8b109d81d
SzymekN <nowak.szymek00@gmail.com>
2022-12-12 13:16:38 +0100

    v1 passed

--- front/assets/js/login.js
+++ front/assets/js/login.js
@@ -5,7 +5,7 @@ const fetchPromise=chkLogin(t, 3, 1000).then((response) => response.json())
 .then((data) => {
   console.log(data)
   localStorage.setItem("token",data.token)
-  getAllCars()
+  loginSuccess();
  
 }).catch( err => {
     console.log('error: '+ err)
@@ -52,53 +52,3 @@ async function loginSuccess(){
   // });
   // fetchPromise;
 //}
-
-function getAllCars() {
-  // const login = {
-  //   email: document.getElementById('email').value,
-  //   password: document.getElementById('password').value
-  // }
-  var target="http://192.168.33.50:8200/api/v1/vehicles/all";
-  const getData=new Promise(async (res, rej) => {                       // return a promise
-    await fetch(target, {method: "GET",mode: 'cors',
-    headers: {
-      "Content-Type": "application/json; charset=UTF-8",
-      "Content-Length":"217",
-      "Authorization":"Bearer "+localStorage.getItem("token")
-    }}).then(async (r) => {   // fetch the resourse
-      // const isJson = r.headers.get('content-type')?.includes('application/json')
-      const data =  await r.json();
-      if(!r.ok)
-      {
-        const error = (data && data.message) || r.status;
-        return Promise.reject(error);
-      }
-        //res(r);                                      // resolve promise if success
-        return res(data);
-    }).then(res.toString).catch( err => {
-        return rej(err);                         // don't try again 
-    });                                              // again until no more tries
-});
-
-getData.then(data=>{
-  localStorage.setItem("allCars",JSON.stringify(data))
-  console.log(JSON.stringify(data))
-  loginSuccess();
-  makeFilters(data);
-}).catch(err=>console.log(err));
-}
-
-function makeFilters(data){
-  //const data = new Map(Object.entries(JSON.parse(jsonData)));
-  const brand = [...new Set(data.map(item => item.brand))];
-  const model = [...new Set(data.map(item => item.model))];
-  const type = [...new Set(data.map(item => item.type))]; 
-  const color = [...new Set(data.map(item => item.color))];
-  const map1= new Map();
-  map1.set('brand',brand);
-  map1.set('model',model);
-  map1.set('type',type);
-  map1.set('color',color);
-  console.log(map1.get('brand'));
-  localStorage.setItem('allFilters',JSON.stringify(Array.from(map1.entries())));
-}
--- front/assets/js/product-page.js
+++ front/assets/js/product-page.js
@@ -89,3 +89,54 @@ function createFOption(filterMap,buttonName,name,listName){
         document.getElementById(listName).appendChild(a);
     }
 }
+
+
+function makeFilters(data){
+    //const data = new Map(Object.entries(JSON.parse(jsonData)));
+    const brand = [...new Set(data.map(item => item.brand))];
+    const model = [...new Set(data.map(item => item.model))];
+    const type = [...new Set(data.map(item => item.type))]; 
+    const color = [...new Set(data.map(item => item.color))];
+    const map1= new Map();
+    map1.set('brand',brand);
+    map1.set('model',model);
+    map1.set('type',type);
+    map1.set('color',color);
+    console.log(map1.get('brand'));
+    localStorage.setItem('allFilters',JSON.stringify(Array.from(map1.entries())));
+}
+
+  
+function getAllCars() {
+    // const login = {
+    //   email: document.getElementById('email').value,
+    //   password: document.getElementById('password').value
+    // }
+    var target="http://192.168.33.50:8200/api/v1/vehicles/all";
+    const getData=new Promise(async (res, rej) => {                       // return a promise
+      await fetch(target, {method: "GET",mode: 'cors',
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Content-Length":"217",
+        "Authorization":"Bearer "+localStorage.getItem("token")
+      }}).then(async (r) => {   // fetch the resourse
+        // const isJson = r.headers.get('content-type')?.includes('application/json')
+        const data =  await r.json();
+        if(!r.ok)
+        {
+          const error = (data && data.message) || r.status;
+          return Promise.reject(error);
+        }
+          //res(r);                                      // resolve promise if success
+          return res(data);
+      }).then(res.toString).catch( err => {
+          return rej(err);                         // don't try again 
+      });                                              // again until no more tries
+  });
+  
+  getData.then(data=>{
+    localStorage.setItem("allCars",JSON.stringify(data))
+    console.log(JSON.stringify(data))
+    makeFilters(data);
+  }).catch(err=>console.log(err));
+}
