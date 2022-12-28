70969d78dc5123fddc173ffc28b658edf817763b
SzymekN <nowak.szymek00@gmail.com>
2022-12-28 15:57:53 +0100

    #14 github issue - new routes and fixes

--- backend/pkg/controller/users_controller.go
+++ backend/pkg/controller/users_controller.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"math/rand"
+	"net/http"
 
 	"github.com/SzymekN/Car-rental-app/pkg/auth"
 	"github.com/SzymekN/Car-rental-app/pkg/executor"
@@ -34,6 +36,8 @@ func (uh *UserHandler) RegisterRoutes() {
 	uh.group.PUT("/users", uh.Update, uh.authConf.IsAuthorized)
 	uh.group.DELETE("/users", uh.Delete, uh.authConf.IsAuthorized)
 	uh.group.PUT("/clients/update/password", uh.UpdatePassword, uh.authConf.IsAuthorized)
+	uh.group.PUT("/users/block", uh.BlockUser, uh.authConf.IsAuthorized)
+	uh.group.PUT("/users/unblock", uh.UnblockUser, uh.authConf.IsAuthorized)
 }
 
 func (uh *UserHandler) Save(c echo.Context) error {
@@ -59,6 +63,98 @@ func (uh *UserHandler) GetById(c echo.Context) error {
 func (uh *UserHandler) GetAll(c echo.Context) error {
 	d, l := executor.GenericGetAll(c, uh.sysOperator, []model.User{})
 	return HandleRequestResult(c, d, l)
+}
+func (uh *UserHandler) BlockUser(c echo.Context) error {
+	// pobranie id
+	// zmiana hasła na jakieś generyczne
+
+	logger := uh.sysOperator.SystemLogger
+	logger.Log = producer.Log{}
+	prefix := fmt.Sprintf("BlockUser ")
+
+	defer func() {
+		logger.Log.Msg = fmt.Sprintf("%s %s", prefix, logger.Log.Msg)
+		logger.ProduceWithJSON(c)
+	}()
+
+	uid := model.UIDWrapper{}
+	uid, logger.Log = BindAny(c, uid)
+
+	db := uh.sysOperator.DB
+	result := db.Debug().Model(&model.User{}).Where("id=?", uid.UID).Update("password", "block")
+	logger.Log = executor.CheckResultError(result)
+	if logger.Log.Err != nil {
+		return logger.Log.Err
+	}
+
+	logger.Log = executor.CheckIfAffected(result)
+	if logger.Log.Err != nil {
+		return logger.Log.Err
+
+	}
+
+	logger.Log.Code = http.StatusOK
+	logger.Log.Key = "info"
+	logger.Log.Msg = fmt.Sprintf("[INFO] user blocked, id: {%v} HTTP: %v", uid.UID, logger.Log.Code)
+	return c.JSON(logger.Log.Code, logger.Log.Msg)
+}
+
+func randomPassword() string {
+	//33 - 126 valid ascii characters
+	var min int64 = 65 // '!'
+	var max int64 = 90 // '~'
+	len := 8
+	pwd := make([]byte, len)
+	for i := 0; i < len; i++ {
+		pwd[i] = byte(rand.Int63n(max-min) + min)
+	}
+
+	return string(pwd)
+}
+
+func (uh *UserHandler) UnblockUser(c echo.Context) error {
+	logger := uh.sysOperator.SystemLogger
+	logger.Log = producer.Log{}
+	prefix := fmt.Sprintf("Unblock User ")
+	var hashedPassword string
+
+	defer func() {
+		logger.Log.Msg = fmt.Sprintf("%s %s", prefix, logger.Log.Msg)
+		logger.ProduceWithJSON(c)
+	}()
+
+	uid := model.UIDWrapper{}
+	uid, logger.Log = BindAny(c, uid)
+
+	if logger.Err != nil {
+		return logger.Err
+	}
+
+	newPassword := randomPassword()
+	hashedPassword, logger.Log = auth.GeneratehashPassword(newPassword)
+	if logger.Err != nil {
+		return logger.Err
+	}
+
+	db := uh.sysOperator.DB
+	result := db.Debug().Model(&model.User{}).Where("id=?", uid.UID).Update("password", hashedPassword)
+	logger.Log = executor.CheckResultError(result)
+	if logger.Log.Err != nil {
+		return logger.Log.Err
+	}
+
+	logger.Log = executor.CheckIfAffected(result)
+	if logger.Log.Err != nil {
+		return logger.Log.Err
+
+	}
+
+	logger.Log.Code = http.StatusOK
+	logger.Log.Key = "info"
+	logger.Log.Msg = fmt.Sprintf("[INFO] user unblocked, id: {%v} HTTP: %v", uid.UID, logger.Log.Code)
+	pwdWrapper := passwordWrapper{newPassword}
+	return c.JSON(logger.Log.Code, pwdWrapper)
+
 }
 
 func (uh *UserHandler) UpdatePassword(c echo.Context) error {
