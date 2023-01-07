15cf3228f783139b3ac0046dccbc64f59071f9f9
Michał Twaróg <michalix1275@gmail.com>
2023-01-07 22:54:41 +0100

    approving repairs

--- front/assets/js/repairs.js
+++ front/assets/js/repairs.js
@@ -8,7 +8,8 @@ async function loadNotificationRepairs(){
         for (i = currentPage* maxReports; i < (currentPage* maxReports)+ maxReports; i++) {
             if(i<Object.keys(reportsInfo).length){
                 // to add when vehicle is in repair do not show
-                //var repair=await getInfoWithBody("")
+                
+                //await getInfoWithBody("http://192.168.33.50:8200/api/v1/notifications")
                 if(typeof reportsInfo[i].vehicle_id!=="undefined"){
                     console.log(reportsInfo[i])
                 let elem = document.createElement('tr');
@@ -29,8 +30,22 @@ async function loadNotificationRepairs(){
         }
     }
 }
-async function addRepair(idVal){
-
+function getReportId(idVal){
+    localStorage.setItem("currentReport",idVal)
+}
+async function addRepair(){
+    var costVal=document.getElementById("cost").value;
+    var currentReportId=localStorage.getItem("currentReport");
+    var notificationInfo=await getInfoWithBody("http://192.168.33.50:8200/api/v1/notifications/info","POST",{id:parseInt(currentReportId)});
+    const repairInfo={
+        cost:parseInt(costVal),
+        notification_id:parseInt(currentReportId),
+        approved:1,
+        vehicle_id:parseInt(notificationInfo.vehicle_id)
+    }
+    console.log(repairInfo)
+    await getInfoWithBody("http://192.168.33.50:8200/api/v1/repairs","POST",repairInfo);
+    reload();
 }
 async function loadRepairs(idVal){
     var repairsInfo=await getInfoWithoutBody("http://192.168.33.50:8200/api/v1/repairs/all","GET");
@@ -40,7 +55,7 @@ async function loadRepairs(idVal){
             if(i<Object.keys(repairsInfo).length){
                 if(typeof repairsInfo[i].vehicle_id!=="undefined"){
                 let elem = document.createElement('tr');
-                elem.append(tmp5.content.cloneNode(true));
+                elem.append(tmp6.content.cloneNode(true));
                 let td=document.createElement('td');
                 
 
